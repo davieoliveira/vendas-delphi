1dc982f91465f6fda6d25c7a8a58672d9042d725
davieoliveira <emanuel.davi@escolar.ifrn.edu.br>
2026-02-02 13:42:41 -0300

    Configurações iniciais

--- view/View.Principal.pas
+++ view/View.Principal.pas
@@ -4,10 +4,25 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus;
 
 type
-  TForm1 = class(TForm)
+  TViewPrincipal = class(TForm)
+    MainMenu1: TMainMenu;
+    Cadastros1: TMenuItem;
+    Movimentaes1: TMenuItem;
+    Financeiro1: TMenuItem;
+    Fiscal1: TMenuItem;
+    Empresas1: TMenuItem;
+    Cidades1: TMenuItem;
+    Pessoas1: TMenuItem;
+    Pessoas2: TMenuItem;
+    SubgruposdeProdutos1: TMenuItem;
+    Produtos1: TMenuItem;
+    Vendas1: TMenuItem;
+    ContasaReceber1: TMenuItem;
+    Configuraes1: TMenuItem;
+    NFCe1: TMenuItem;
   private
     { Private declarations }
   public
@@ -15,7 +30,7 @@ type
   end;
 
 var
-  Form1: TForm1;
+  ViewPrincipal: TViewPrincipal;
 
 implementation
 
